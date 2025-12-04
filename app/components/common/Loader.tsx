2570bd77fc420ace00450c832f9728c9bdfdc54a
devendra <devendra@sourceved.com>
2025-12-04 13:02:23 +0530

    comment removed

--- app/components/common/Loader.tsx
+++ app/components/common/Loader.tsx
@@ -5,11 +5,6 @@ const Loader = ({
   className?: string;
   progress?: number;
 }) => {
-  // Calculate circumference and offset for the progress ring
-  //const radius = 45;
-  //const circumference = 2 * Math.PI * radius;
-  //const progressOffset = circumference - (progress / 100) * circumference;
-
   return (
     <div
       className={`absolute top-0 left-0 w-full h-full
@@ -21,40 +16,6 @@ const Loader = ({
     >
       <div className="w-[72px] h-[72px]  rounded-[72.493px]  border-[11px] border-white border-t-transparent  bg-transparent  animate-spin  box-border  "></div>
     </div>
-
-    // <div role="status" className={`flex items-center justify-center ${className} bg-transparent w-full relative overflow-hidden`}>
-    //     <svg
-    //         aria-hidden="true"
-    //         className="w-20 h-20 animate-spin"
-    //         viewBox="0 0 100 100"
-    //         fill="none"
-    //         xmlns="http://www.w3.org/2000/svg"
-    //         style={{ animationDuration: '2s' }}
-    //     >
-    //         {/* Background white ring */}
-    //         <circle
-    //             cx="50"
-    //             cy="50"
-    //             r={radius}
-    //             stroke="white"
-    //             strokeWidth="10"
-    //             fill="none"
-    //         />
-    //         {/* Progress gray segment */}
-    //         <circle
-    //             cx="50"
-    //             cy="50"
-    //             r={radius}
-    //             stroke="#9CA3AF"
-    //             strokeWidth="10"
-    //             fill="none"
-    //             strokeDasharray={circumference}
-    //             strokeDashoffset={progressOffset}
-    //             transform="rotate(-90 50 50)"
-    //         />
-    //     </svg>
-    //     <span className="sr-only">Loading...</span>
-    // </div>
   );
 };
 
